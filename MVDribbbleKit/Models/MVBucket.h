30708957155faec465f58688b115fdf647ab99e1
Marcel Voss <marcel.voss@icloud.com>
2014-09-27 21:53:08 +0200

    Added a new class for buckets.

---
+++ MVDribbbleKit/Models/MVBucket.h
@@ -0,0 +1,13 @@
+//
+//  MVBucket.h
+//  DribbbleTest
+//
+//  Created by Marcel Voß on 27.09.14.
+//  Copyright (c) 2014 Marcel Voss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MVBucket : NSObject
+
+@end
---
+++ MVDribbbleKit/Models/MVBucket.m
@@ -0,0 +1,13 @@
+//
+//  MVBucket.m
+//  DribbbleTest
+//
+//  Created by Marcel Voß on 27.09.14.
+//  Copyright (c) 2014 Marcel Voss. All rights reserved.
+//
+
+#import "MVBucket.h"
+
+@implementation MVBucket
+
+@end
