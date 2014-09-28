9c09dc975f5ab9f867cd76bda2ec974fdd376339
Marcel Voss <marcel.voss@icloud.com>
2014-09-28 12:27:35 +0200

    MVProject class added.

---
+++ MVDribbbleKit/Models/MVProject.m
@@ -0,0 +1,13 @@
+//
+//  MVProject.m
+//  DribbbleTest
+//
+//  Created by Marcel Voß on 28.09.14.
+//  Copyright (c) 2014 Marcel Voss. All rights reserved.
+//
+
+#import "MVProject.h"
+
+@implementation MVProject
+
+@end
