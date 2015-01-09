7780443f86b3c5dab24ccf2f6d54683aa90d8366
Marcel Voss <marcel.voss@icloud.com>
2015-01-09 23:42:23 +0100

    Updated the copyright.

--- MVDribbbleKit/Models/MVLike.h
+++ MVDribbbleKit/Models/MVLike.h
@@ -1,6 +1,6 @@
 // MVLike.h
 //
-// Copyright (c) 2014 Marcel Voss
+// Copyright (c) 2014-2015 Marcel Voss
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
